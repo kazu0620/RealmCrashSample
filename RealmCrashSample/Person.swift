749f7e21ed7de7a0d47b93c97c64dcade8085259
Kazuhiro Sakamoto <kazu620@gmail.com>
2016-03-09 11:07:36 +0900

    initia

---
+++ RealmCrashSample/Person.swift
@@ -0,0 +1,21 @@
+//
+//  Person.swift
+//  RealmCrashSample
+//
+//  Created by sakamoto kazuhiro on 2016/03/09.
+//  Copyright © 2016年 Sakamoto Kazuhiro. All rights reserved.
+//
+class Person: RLMObject{
+    dynamic var name = ""
+    var myPet:Animal?
+    
+    override static func ignoredProperties() -> [String]? {
+        return ["myPet"]
+    }
+}
+
+@objc protocol Animal {
+}
+
+class Dog:Animal {
+}
